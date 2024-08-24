e9756f190d13f514162fdbe4d24a46f7a6d68854
Varun Yadav <vy056038@gmail.com>
2024-08-24 10:08:50 +0530

    Basic server setup done

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,27 @@
-console.log("Hello")
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(urlencoded({ extended: true }));
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+app.get("/",(req,res)=>{
+    return res.status(200).json({
+        message:"Hello from server",
+        success:true
+    })
+})
+
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port:${PORT}`);
+});
