f5507e1fdc5994fc0e549620280281ce59996fbe
Varun Yadav <vy056038@gmail.com>
2024-08-26 16:52:20 +0530

    getMessage controller done

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -36,3 +36,25 @@ export const sendMessage = async (req, res) => {
     console.log(error);
   }
 };
+
+export const getMessage = async (req, res) => {
+  try {
+    const senderId = req.id;
+    const receiverId = req.params.id;
+    const conversation = await Conversation.find({
+      participants: { $all: [senderId, receiverId] },
+    });
+    if (!conversation) {
+      return res.status(200).json({
+        messages: [],
+        success: true,
+      });
+    }
+    return res.status(200).json({
+      messages: conversation?.messages,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
