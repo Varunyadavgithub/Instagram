413d12041ee365a659ff46f1f9c61d350243df94
Varun Yadav <vy056038@gmail.com>
2024-08-26 16:44:11 +0530

    sendMessage controller done

--- backend/models/conversation.model.js
+++ backend/models/conversation.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const conversationSchema = new mongoose.Schema(
   {
     participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    message: [{ type: mongoose.Schema.Types.ObjectId, ref: "Message" }],
+    messages: [{ type: mongoose.Schema.Types.ObjectId, ref: "Message" }],
   },
   { timestamps: true }
 );
