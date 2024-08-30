3a139a02fec93aff3c47c6e440d6787efa6c43ae
Varun Yadav <vy056038@gmail.com>
2024-08-30 12:08:31 +0530

    redux setup done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-    <Toaster />
+    <Provider store={store}>
+      <App />
+      <Toaster />
+    </Provider>
   </StrictMode>
 );
