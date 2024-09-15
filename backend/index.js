a525a30c500e88b80b6b7d36e1449bbfce74eda4
Varun Yadav <vy056038@gmail.com>
2024-09-15 15:45:11 +0530

    scoket io implementation and added some events done

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,9 @@ import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import postRoute from "./routes/post.route.js";
 import messageRoute from "./routes/message.route.js";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config({});
-const app = express();
 
 // middlewares
 app.use(express.json());
@@ -33,7 +33,7 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectDB();
   console.log(`Server is running on port:${PORT}`);
 });
